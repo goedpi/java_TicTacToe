8bc70201e56d2bbbeab6194a9bcf632e61eed716
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-19 12:10:57 +0200

    reversed board to match numpad layout

--- tictactoe/src/main/java/com/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/com/tictactoe/TicTacToe.java
@@ -21,11 +21,12 @@ public class TicTacToe {
         
     }
     private static void printTheBoard(char[] board){
-        System.out.println( board[0] + " | " +  board[1] + " | " + board[2]);
+        System.out.println( board[6] + " | " +  board[7] + " | " + board[8]);
         System.out.println( " - + - + - " );
         System.out.println( board[3] + " | " +  board[4] + " | " + board[5]);
         System.out.println( " - + - + - " );
-        System.out.println( board[6] + " | " +  board[7] + " | " + board[8]);
+        System.out.println( board[0] + " | " +  board[1] + " | " + board[2]);
+        
     }
 
     
