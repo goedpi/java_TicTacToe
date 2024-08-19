968762d7f909008976f323bed847d358d711891e
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-19 12:00:40 +0200

    Printing board in console added

--- tictactoe/src/main/java/com/tictactoe/TicTacToe.java
+++ tictactoe/src/main/java/com/tictactoe/TicTacToe.java
@@ -2,6 +2,22 @@ package com.tictactoe;
 
 public class TicTacToe {
     public static void main(String[] args) {
+        char[] board = {'1', '2','3',
+                        '4', '5','6',
+                        '7', '8', '9'};
+        var NumSquaresPlayed = 0;
+        var WhosTurn = 'x';
+
+        printTheBoard(board);
         
     }
+    private static void printTheBoard(char[] board){
+        System.out.println( board[0] + " | " +  board[1] + " | " + board[2]);
+        System.out.println( " - + - + - " );
+        System.out.println( board[3] + " | " +  board[4] + " | " + board[5]);
+        System.out.println( " - + - + - " );
+        System.out.println( board[6] + " | " +  board[7] + " | " + board[8]);
+    }
+
+    
 }
