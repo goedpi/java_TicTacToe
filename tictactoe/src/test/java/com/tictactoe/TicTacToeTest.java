ff2cedbdb925681cf919dd2d9550861a5777db93
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-20 11:40:53 +0200

    refactored tests to utilized player object

--- tictactoe/src/test/java/com/tictactoe/TicTacToeTest.java
+++ tictactoe/src/test/java/com/tictactoe/TicTacToeTest.java
@@ -7,52 +7,47 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.io.ByteArrayInputStream;
 import java.util.Scanner;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TicTacToeTest {
-   @Test
+    player currentPlayer;
+    @BeforeEach
+    public void setupPlayer(){
+        String input = "John\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(in);
+        player player1 = new player(scanner);
+        player1.configurePlayer(1, 'x');
+        currentPlayer = player1;
+    }
+    
+    @Test
     public void testCheckWinnerRow() {
         char[] board = {'x', 'x', 'x', '4', '5', '6', '7', '8', '9'};
-        char whosTurn = 'x';
         
-        int result = TicTacToe.checkWinner(board, whosTurn, 3);
+        int result = TicTacToe.checkWinner(board, currentPlayer, 3);
         assertEquals(1, result, "Player X should win by completing the first row.");
     }
     @Test
     public void testCheckWinnerDiagonal(){
         char[] board = {'x', '2', '3', '4', 'x', '6', '7', '8', 'x'};
-        char whosTurn = 'x';
         
-        int result = TicTacToe.checkWinner(board, whosTurn, 3);
+        int result = TicTacToe.checkWinner(board, currentPlayer, 3);
         assertEquals(1, result, "Player X should win by completing the first row.");
     }
     @Test
     public void testCheckWinnerColumn(){
         char[] board = {'1', 'x', '3', '4', 'x', '6', '7', 'x', '9'};
-        char whosTurn = 'x';
         
-        int result = TicTacToe.checkWinner(board, whosTurn, 3);
+        int result = TicTacToe.checkWinner(board, currentPlayer, 3);
         assertEquals(1, result, "Player X should win by completing the first row.");
     }
     @Test public void noWinner(){
         char[] board = {'x', 'o', 'x', 'x', '0', '0', '7', '8', '9'};
-        char whosTurn = 'x';
-        int result = TicTacToe.checkWinner(board, whosTurn, 6);
+        int result = TicTacToe.checkWinner(board, currentPlayer, 6);
         assertEquals(0, result, "No one should've won");
     }   
-
-    @Test public void playerInputValid(){
-        char[] boardInput = {'x', '2', '3', '4', 'x', '6', '7', '8', 'x'};
-        char whosTurn = 'x';
-        
-        char[] boardExpected = {'x', '2', '3', 'x', 'x', '6', '7', '8', 'x'};
-        String input = "4\n";
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
-        Scanner testScanner = new Scanner(in);
-        
-        TicTacToe.playerInput(boardInput, whosTurn, testScanner);
-        assertArrayEquals(boardExpected, boardInput);
-
     }
     
-}
+
