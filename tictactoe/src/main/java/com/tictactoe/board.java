c83657bb08b9ea9fee8261422bf21637c5bdfcf6
VALORI\PimGoedegebuure <pimgoedegebuure@valori.nl>
2024-08-20 13:32:32 +0200

    added board object and tests

---
+++ tictactoe/src/main/java/com/tictactoe/board.java
@@ -0,0 +1,26 @@
+package com.tictactoe;
+
+public class board {
+    player currentPlayer;
+    private char[] board = { '1', '2', '3',
+                             '4', '5', '6',
+                             '7', '8', '9' };
+    public char[] getBoard(){
+        return board;
+    }
+
+    public void setSquare(int index, player currentPlayer){
+        board[index] = currentPlayer.getSymbol();
+    }
+
+    public char getSquare(int index){
+        return board[index];
+    }
+    public void printBoard(){
+        System.out.println(board[6] + " | " + board[7] + " | " + board[8]);
+        System.out.println(" - + - + - ");
+        System.out.println(board[3] + " | " + board[4] + " | " + board[5]);
+        System.out.println(" - + - + - ");
+        System.out.println(board[0] + " | " + board[1] + " | " + board[2]);
+    }
+}
---
+++ tictactoe/src/test/java/com/tictactoe/boardTest.java
@@ -0,0 +1,45 @@
+package com.tictactoe;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class boardTest {
+    player currentPlayer;
+    @BeforeEach
+    public void setupPlayer(){
+        String input = "John\n";
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        Scanner scanner = new Scanner(in);
+        player player1 = new player(scanner);
+        player1.configurePlayer(1, 'x');
+        currentPlayer = player1;
+    }
+    
+    
+    board Board = new board();
+    char[] defaultBoard = { '1', '2', '3','4', '5', '6','7', '8', '9' };
+
+    @Test
+    public void getBoard(){
+        //validates that board is set to expected default
+        assertArrayEquals(Board.getBoard(),defaultBoard, "initial board matches is as expected");
+    }
+    @Test
+    public void setSquareTest(){
+        char[] expectedBoard = { '1', 'x', 'x','4', '5', '6','7', '8', '9' };
+        Board.setSquare(1, currentPlayer);
+        Board.setSquare(2, currentPlayer);
+        assertArrayEquals(Board.getBoard(), expectedBoard);
+    }
+    @Test
+    public void getSquareTest(){
+        assertEquals(Board.getSquare(4), '5');
+    }
+}
